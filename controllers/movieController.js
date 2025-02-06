e3afab583b0c4f6f9d32c1e5a600c487bf3519ea
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-06 15:43:12 +0100

    Enhance movie retrieval to include associated reviews in the response

--- controllers/movieController.js
+++ controllers/movieController.js
@@ -12,11 +12,21 @@ const show = (req, res) => {
   const id = req.params.id;
 
   const sqMovie = "SELECT * FROM movies WHERE id = ?";
+  const sqReview = "SELECT * FROM reviews WHERE movie_id = ?";
+
   connection.query(sqMovie, [id], (error, results) => {
     if (error) return res.status(500).json({ error: "Errore nella query" });
     if (results.lenght === 0)
       return res.status(404).json({ error: "Film non trovato" });
     res.json(results[0]);
+  });
+
+  let movie = results[0];
+
+  connection.query(sqReview, [id], (error, reviewResults) => {
+    if (error) return res.status(500).json({ error: "Errore nella query" });
+    movie.reviews = reviewResults;
+    res.json(movie);
   });
 };
 
