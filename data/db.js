21f191dfd8a42674141f1e1ef2de7bd5cadc6caf
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-06 15:13:43 +0100

    Update environment variable names for database connection in .env and db.js

--- data/db.js
+++ data/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.HOST,
+  user: process.env.USER,
+  password: process.env.PASSWORD,
+  database: process.env.NAME,
 });
 
 connection.connect((err) => {
