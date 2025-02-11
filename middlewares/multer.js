73c2de588efb0c60a51382aa210022b0f33e86a1
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-11 15:20:29 +0100

    feat: add movie cover images and update multer storage destination path

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -3,7 +3,7 @@ const multer = require("multer");
 //upload immagine
 
 const storage = multer.diskStorage({
-  destination: ".public/movies_cover",
+  destination: "public/movies_cover",
   filename(req, file, cb) {
     const uniqueName = `${Date.now()}-${file.originalname}`;
     cb(null, uniqueName);
