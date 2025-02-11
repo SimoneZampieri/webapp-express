824518c3ccc82a1a6568eb64bc257deced831bce
SimoneZampieri <simonezampieri106@gmail.com>
2025-02-11 11:45:51 +0100

    fix: update multer filename function syntax to use method shorthand

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -4,10 +4,10 @@ const multer = require("multer");
 
 const storage = multer.diskStorage({
   destination: ".public/movies_cover",
-  filename (req, file, cb) => {
+  filename(req, file, cb) {
     const uniqueName = `${Date.now()}-${file.originalname}`;
     cb(null, uniqueName);
-  }
+  },
 });
 
 const upload = multer({ storage });
